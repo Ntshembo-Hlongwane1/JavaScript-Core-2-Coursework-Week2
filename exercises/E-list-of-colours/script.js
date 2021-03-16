1cb1a7541de37ceb52dc7eb16f43e408ea946df8
Junior Hlongwane <juniorWebProjects@gmail.com>
2021-03-16 18:39:17 +0200

    Add: Exercises color change

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,7 +1,17 @@
-function listOfColours(colours) {
-  // Write your code here...
+let colorBackground = document.getElementById("main")[0];
+function changeBackground1() {
+  setTimeout(function () {
+    main.style.backgroundColor = "red";
+  }, 5001);
 }
-
-const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
-
-listOfColours(colours);
+changeBackground1();
+let colorArray = ["gold", "crimson", "turquoise", "magenta"];
+function changeBackground2() {
+  let x = 0;
+  setInterval(() => {
+    if (x == colorArray.length) x = 0;
+    main.style.backgroundColor = colorArray[x];
+    x++;
+  }, 5000);
+}
+changeBackground2();
